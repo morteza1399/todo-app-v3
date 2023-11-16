821d5244a590181a4ff334252c01555d2fb35d58
morteza <m.helan71@gmail.com>
2023-11-16 15:30:18 +0330

    style: filter box responsive

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="min-h-screen bg-no-repeat bg-top xl:dark:bg-desktop-dark lg:dark:bg-desktop-dark md:dark:bg-desktop-dark sm:dark:bg-desktop-dark xs:dark:bg-mobile-dark xxs:dark:bg-mobile-dark dark:bg-dark-200 xl:bg-desktop-light lg:bg-desktop-light md:bg-desktop-light sm:bg-desktop-light bg-white-200 xs:bg-mobile-light bg-white-200 xxs:bg-mobile-light"
+    class="min-h-screen bg-no-repeat bg-top xl:dark:bg-desktop-dark lg:dark:bg-desktop-dark md:dark:bg-desktop-dark sm:dark:bg-desktop-dark xs:dark:bg-mobile-dark xxs:dark:bg-mobile-dark dark:bg-dark-200 xl:bg-desktop-light lg:bg-desktop-light md:bg-desktop-light sm:bg-desktop-light xl:bg-white-400 lg:bg-white-400 md:bg-white-400 sm:bg-white-200 xs:bg-white-200 xxs:bg-white-200 xs:bg-mobile-light xxs:bg-mobile-light"
     ref="content"
   >
     <div class="flex justify-between items-center pt-24 responsive-width">
