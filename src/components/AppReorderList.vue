ddd6234ced9b6bea6905eac426e447e584bbf0b1
morteza <m.helan71@gmail.com>
2023-12-29 22:03:49 +0330

    refactore: write better appReorderList component

--- src/components/AppReorderList.vue
+++ src/components/AppReorderList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-centered text-gray-100 text-sm py-7 mx-auto h-3/3">
+  <div class="flex-centered text-gray-100 text-sm py-7 mx-auto h-full">
     <p>Drag and drop to reorder list</p>
   </div>
 </template>
