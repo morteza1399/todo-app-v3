b09dfaf14a869c8a0f2d141056a69fe257a19b4d
morteza <m.helan71@gmail.com>
2023-12-29 21:58:04 +0330

    refactore: write better appFilter component

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -2,14 +2,13 @@
   <div>
     <a
       href="#"
-      :class="`${
-        currentIndex === index
-          ? 'dark:filter-link filter-link-light text-blue-200'
-          : 'dark:filter-link filter-link-light'
-      }`"
       v-for="(filterItem, index) in filterItems"
       :key="index"
-      @click.prevent="filterTodoItem(filterItem)"
+      :class="{
+        'dark:filter-link filter-link-light': true,
+        'text-blue-200': currentIndex === index,
+      }"
+      @click.prevent="filterTodoItem(filterItem, index)"
     >
       {{ filterItem }}
     </a>
@@ -17,18 +16,27 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useTodoStore } from "@/pinia/todo";
 
 const store = useTodoStore();
 const filterItems = ref(["All", "Active", "Complete"]);
 const currentIndex = ref(+localStorage.getItem("current-index"));
 
-function filterTodoItem(item) {
+function filterTodoItem(item, index) {
   localStorage.setItem("filter-status", item);
   store.setFilterStatus(item);
-  let key = filterItems.value.indexOf(item);
-  currentIndex.value = key;
-  localStorage.setItem("current-index", currentIndex.value);
+  currentIndex.value = index;
+  localStorage.setItem("current-index", index);
 }
+
+onMounted(() => {
+  // Ensure currentIndex is a valid index
+  if (
+    currentIndex.value < 0 ||
+    currentIndex.value >= filterItems.value.length
+  ) {
+    currentIndex.value = 0;
+  }
+});
 </script>
