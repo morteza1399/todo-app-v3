61eb17e223c08074f7e6709575996b5048c6298a
morteza <m@yahooc.com>
2024-01-28 11:35:00 +0330

    refactore: use enum type in AppConstants file

--- src/utils/AppConstants.ts
+++ src/utils/AppConstants.ts
@@ -1,19 +1,19 @@
-export const ThemeConstants = {
-  DARK_THEME_CLASS: "dark",
-  LOCAL_STORAGE_THEME_KEY: "theme",
-  SUN_ICON: "sun",
-  MOON_ICON: "moon",
-};
+export enum ThemeConstants {
+  DARK_THEME_CLASS = "dark",
+  LOCAL_STORAGE_THEME_KEY = "theme",
+  SUN_ICON = "sun",
+  MOON_ICON = "moon",
+}
 
-export const APIConstants = {
-  BASE_URL: "http://localhost:3000",
-};
+export enum APIConstants {
+  BASE_URL = "http://localhost:3000",
+}
 
-export const TodoItemsConstants = {
-  LOCAL_STORAGE_TODOITEM_KEY: "all-todo-items",
-};
+export enum TodoItemsConstants {
+  LOCAL_STORAGE_TODOITEM_KEY = "all-todo-items",
+}
 
-export const FilterConstants = {
-  LOCAL_STORAGE_INDEX_KEY: "current-index",
-  LOCAL_STORAGE_FILTER_KEY: "filter-status",
-};
+export enum FilterConstants {
+  LOCAL_STORAGE_INDEX_KEY = "current-index",
+  LOCAL_STORAGE_FILTER_KEY = "filter-status",
+}
